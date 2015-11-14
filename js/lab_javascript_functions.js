ac42371c323d07335539b4f5afacd17e4d9053d6
Daniel Petro <DanielPetro@Daniels-MacBook-Pro.local>
2015-11-14 08:33:20 -0500

    fixed which is smaller js, still confused on concatenate

--- js/lab_javascript_functions.js
+++ js/lab_javascript_functions.js
@@ -2,3 +2,5 @@ var firstVariable = alert("Type something");
 
 console.log("firstVariable is: ", firstVariable);
 console.log("firstVariable type is: ", typeof firstVariable);
+
+prompt()
--- js/which_is_smaller.js
+++ js/which_is_smaller.js
@@ -2,14 +2,34 @@ function whichIsSmaller() {
   var firstNumber = document.getElementById("firstNumber").value;
   var secondNumber = document.getElementById("secondNumber").value;
 
+  console.log("firstNumber is: ", firstNumber);
+  console.log("firstNumber type is: ", typeof firstNumber);
+
+  console.log("secondNumber is ", secondNumber);
+  console.log("secondNumber type is: ", typeof secondNumber);
+
+
+  firstNumber = parseInt(firstNumber);
+  secondNumber = parseInt(secondNumber);
+
+  if(isNaN(firstNumber)) {
+    alert("You must enter a number(#) in the first box");
+  }
+  if(isNaN(secondNumber)) {
+    alert("You must enter a number(#) in the second box");
+  }
+
+
+
+
     if(firstNumber < secondNumber) {
-      alert("Your 1st #  is the smaller number");
+      alert(firstNumber);
     } else {
       if(secondNumber < firstNumber) {
-        alert("Your 2nd # is the smaller number" );
+        alert(secondNumber);
       } else {
         if(firstNumber = secondNumber)
-          alert("Please put two numbers that are not the same")
+          alert("Please put a number(#) in each box, or two numbers(#) that are not the same")
       }
     }
   }
